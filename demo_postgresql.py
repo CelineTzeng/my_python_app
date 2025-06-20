6609da0798e182bcfa078ddd4e99236241241f04
CelineTzeng <celincecem@gmail.com>
2025-06-20 16:30:04 +0800

    update to v2

--- demo_postgresql.py
+++ demo_postgresql.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = \
-    "postgresql://admin:123456@127.0.0.1:5432/testdb"
+    "postgresql://admin:1234@127.0.0.1:5432/testdb"
 
 db = SQLAlchemy(app)
 
@@ -21,6 +22,49 @@ class Students(db.Model):
         self.tel = tel
         self.addr = addr
         self.email= email
+
+
+@app.route("/")
+def index():
+    db.create_all()
+    return "資料庫連線成功"
+
+@app.route("/insert")
+def insert():
+    student = Students("炭治郎","0911111111","台北市信義路101號","tjl@gmail.com")
+    db.session.add(student)
+    db.session.commit()
+    return "新增一筆記錄成功"
+
+@app.route("/insertAll")
+def insertAll():
+    student1 = Students("彌豆子","0922222222","台北市南京東路101號","mdj@gmail.com")
+    student2 = Students("伊之助","0933333333","台北市復興北路101號","yjj@gmail.com")
+    student = (student1,student2)
+    db.session.add_all(student)
+    db.session.commit()
+    return "新增多筆記錄成功"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 
 
